5b9636cc7ba7ab4d57d12175fcb45f5121c1a440
Ben O'Neill <ben@oneill.sh>
2026-02-18 12:28:20 -0500

    Grin tests

--- test/test_grin.c
+++ test/test_grin.c
@@ -1,7 +1,7 @@
 #include "unity.h"
 
-#include "interpret.h"
 #include "langs/grin.h"
+#include "langs/util.h"
 #include "util.c"
 
 #define GRIN_DATA ((BFX_GrinData*) bfx.lang_data)
@@ -179,5 +179,226 @@ void test_bfx_op_grin_round(void) {
 }
 
 void test_bfx_op_grin_nand(void) {
-    // TODO
+    INITIALIZE("&");
+    GRIN_DATA->tape[0] = 1.0;
+    GRIN_DATA->reg     = 2.0;
+
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(~(1 & 2), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_or(void) {
+    INITIALIZE("|");
+    GRIN_DATA->tape[0] = 1.0;
+    GRIN_DATA->reg     = 2.0;
+
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(1 | 2, GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_not(void) {
+    INITIALIZE("!");
+    GRIN_DATA->tape[0] = 1.0;
+
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(~1, GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_negate(void) {
+    INITIALIZE("@");
+    GRIN_DATA->tape[0] = 1.0;
+
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(-1.0, GRIN_DATA->tape[0]);
+
+    bfx.ip = 0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(1.0, GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_simplify(void) {
+    INITIALIZE("?");
+
+    GRIN_DATA->tape[0] = 10.5;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(1.0, GRIN_DATA->tape[0]);
+
+    bfx.ip             = 0;
+    GRIN_DATA->tape[0] = -10.5;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(-1.0, GRIN_DATA->tape[0]);
+
+    bfx.ip             = 0;
+    GRIN_DATA->tape[0] = -0.5;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(0.0, GRIN_DATA->tape[0]);
+
+    bfx.ip             = 0;
+    GRIN_DATA->tape[0] = 0.5;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(0.0, GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_inc_t(void) {
+    INITIALIZE("}");
+    GRIN_DATA->tape[0] = 1.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(2.0, GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_dec_t(void) {
+    INITIALIZE("{");
+    GRIN_DATA->tape[0] = 1.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(0.0, GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_loops(void) {
+    INITIALIZE("}}}[>}<{]")
+    bfx_build_loops(&bfx);
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(0.0, GRIN_DATA->tape[0]);
+    TEST_ASSERT_EQUAL(3.0, GRIN_DATA->tape[1]);
+}
+
+void test_bfx_op_grin_print(void) {
+    INITIALIZE("(HELLO WORLD)");
+    REDIRECT_STDOUT;
+    bfx_grin_run(&bfx);
+    RESTORE_STDOUT;
+    TEST_ASSERT_EQUAL_STRING("HELLO WORLD\n", out);
+}
+
+void test_bfx_op_grin_newline(void) {
+    INITIALIZE(")");
+    REDIRECT_STDOUT;
+    bfx_grin_run(&bfx);
+    RESTORE_STDOUT;
+    TEST_ASSERT_EQUAL_STRING("\n", out);
+}
+
+void test_bfx_op_grin_sin(void) {
+    INITIALIZE("s");
+    GRIN_DATA->tape[0] = 0.5;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(sin(0.5), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_cos(void) {
+    INITIALIZE("c");
+    GRIN_DATA->tape[0] = 0.5;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(cos(0.5), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_tan(void) {
+    INITIALIZE("t");
+    GRIN_DATA->tape[0] = 0.5;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(tan(0.5), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_arcsin(void) {
+    INITIALIZE("S");
+    GRIN_DATA->tape[0] = 0.5;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(asin(0.5), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_arccos(void) {
+    INITIALIZE("C");
+    GRIN_DATA->tape[0] = 0.5;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(acos(0.5), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_arctan(void) {
+    INITIALIZE("T");
+    GRIN_DATA->tape[0] = 0.5;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(atan(0.5), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_inv(void) {
+    INITIALIZE("1");
+    GRIN_DATA->tape[0] = 20.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(1 / 20.0, GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_mod2(void) {
+    INITIALIZE("2");
+    GRIN_DATA->tape[0] = 20.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(fmod(20.0, 2), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_e(void) {
+    INITIALIZE("e");
+    GRIN_DATA->tape[0] = 20.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(M_E, GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_pi(void) {
+    INITIALIZE("p");
+    GRIN_DATA->tape[0] = 20.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(M_PI, GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_ln(void) {
+    INITIALIZE("l");
+    GRIN_DATA->tape[0] = 20.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(log(20.0), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_mean(void) {
+    INITIALIZE("m");
+    GRIN_DATA->tape[0] = 20.0;
+    GRIN_DATA->reg     = 10.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL((20.0 + 10.0) / 2, GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_sqrt(void) {
+    INITIALIZE("q");
+    GRIN_DATA->tape[0] = 20.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(sqrt(20.0), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_log(void) {
+    INITIALIZE("L");
+    GRIN_DATA->tape[0] = 20.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(log10(20.0), GRIN_DATA->tape[0]);
+}
+
+void test_bfx_op_grin_jump(void) {
+    INITIALIZE("j");
+    GRIN_DATA->reg = 20.0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(20 + 1, bfx.ip);
+}
+
+void test_bfx_op_grin_toggle_deg_rad(void) {
+    INITIALIZE("D");
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(BFX_GRIN_DEG, GRIN_DATA->unit);
+
+    bfx.ip = 0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(BFX_GRIN_RAD, GRIN_DATA->unit);
+}
+
+void test_bfx_op_grin_exit(void) {
+    INITIALIZE("`{");
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(0.0, GRIN_DATA->tape[0]);
+
+    bfx.ip = 0;
+    bfx_grin_run(&bfx);
+    TEST_ASSERT_EQUAL(BFX_GRIN_RAD, GRIN_DATA->unit);
 }
