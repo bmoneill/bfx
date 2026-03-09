62fd85a9626b9c6a786cb300506d9da04435a42f
Ben O'Neill <ben@oneill.sh>
2026-03-09 12:31:56 -0400

    Add more tests

---
+++ test/test_util.c
@@ -0,0 +1,32 @@
+#include "bfx.h"
+#include "langs/util.h"
+#include "unity.h"
+
+#include "util.c"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_bfx_build_loops(void) {
+    BFX         bfx;
+    const char* program = "[-]";
+    bfx.program         = (char*) program;
+    bfx.program_len     = strlen(program);
+    bfx.program_size    = strlen(program);
+
+    BFX_Error ret       = bfx_build_loops(&bfx);
+
+    TEST_ASSERT_EQUAL_INT(BFX_SYNTAX_ERROR, ret);
+}
+
+void test_bfx_build_loops_WithUnmatchedBrackets(void) {
+    BFX         bfx;
+    const char* program = "[-";
+    bfx.program         = (char*) program;
+    bfx.program_len     = strlen(program);
+    bfx.program_size    = strlen(program);
+
+    BFX_Error ret       = bfx_build_loops(&bfx);
+
+    TEST_ASSERT_EQUAL_INT(BFX_SYNTAX_ERROR, ret);
+}
